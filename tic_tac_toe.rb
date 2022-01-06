0d82d63396c70f46060a04aeb6541a23ae087809
meowzie <appenge2@gmail.com>
2022-01-06 21:55:43 +0200

    added the 'Position' class, which creates new positions (that are then placed on the tic tac toe chart). Also created a variable to store the chart and created an array of positions.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# creates players
+# creates players, stores their names and symbols (X vs O) in variables.
 class Player
-  attr_accessor :name
+  attr_accessor :player
 
   def initialize(player, symbol)
     @player = player
@@ -10,8 +10,28 @@ class Player
   end
 
   def namer
-    puts "\nEnter #{@player}'s name:"
     @player = gets.chomp
-    puts "\nHello, #{@player.capitalize}. You are playing with #{@symbol}."
   end
 end
+
+# creates positions and updates them when they are chosen by the player
+class Position
+  attr_accessor :position
+
+  def initialize(position)
+    @position = position
+  end
+
+  def update_position(symbol)
+    @position = symbol
+  end
+end
+
+positions = [Position.new(1), Position.new(2), Position.new(3), Position.new(4), Position.new(5), Position.new(6),
+             Position.new(7), Position.new(8), Position.new(9)]
+
+chart = " #{positions[0].position} | #{positions[1].position} | #{positions[2].position}
+---+---+---
+ #{positions[3].position} | #{positions[4].position} | #{positions[5].position}
+---+---+---
+ #{positions[6].position} | #{positions[7].position} | #{positions[8].position}"
